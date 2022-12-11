9852d44ca97538ac7821b91e97f361833c84c48a
SVSTN <born.golod@gmail.com>
2022-12-11 21:02:51 +0300

    add assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,9 +1,6 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.deepEqual(capitalize(''), '');
+assert.deepEqual(capitalize('hello'), 'Hello');
 console.log('Все тесты пройдены!');
